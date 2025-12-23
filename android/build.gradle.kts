67f7bd47c13d672ac54f0e52e6d324d38f70fdca
Shivansh Shrivastav <shiva.shri0055@gmail.com>
2025-12-23 22:34:52 +0530

    flutter is connected with firebase and azure

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,6 +4,13 @@ allprojects {
         mavenCentral()
     }
 }
+plugins {
+    // ...
+
+    // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.4.4" apply false
+
+}
 
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
